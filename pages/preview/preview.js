ee71e8df50016fec24f1276714d500c29b81d4bf
crab1127 <crab1127@gmail.com>
2017-03-29 09:43:55 +0800

    Add 身份扫描逻辑

--- pages/preview/preview.js
+++ pages/preview/preview.js
@@ -291,15 +291,27 @@ Page({
       height: imgHeith
     })
   },
-  onChooseImg() {
+  onChooseImg(e) {
+    const current = e.currentTarget.dataset.current
+    const imgUrls = this.data.imgUrls
+
     wx.chooseImage({
       count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: (res) => {
         // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
-        console.log(res)
-
+        // console.log(res)
+        const imgUrl = res.tempFilePaths.join(',')
+        imgUrls[current] = {
+          url: imgUrl,
+          originUrl: imgUrl
+        }
+        this.setData({
+          current: current,
+          imgUrls: imgUrls
+        })
+        this.onEdit()
       }
     })
   }
